ba0f4973b516e6d496838342ae10f21958b4eea2
Nhat <nhat499@uw.edu>
2024-02-28 22:57:55 -0800

    added headers

---
+++ baseComponents/headers.js
@@ -0,0 +1,17 @@
+
+import { Text } from "react-native";
+
+const Headers = ({ children }) => {
+    return (
+        <Text style={{
+            color: "black",
+            fontSize: "20px",
+            fontWeight: "bold",
+        }}>{children}</Text>
+
+
+
+    );
+}
+
+export default Headers;
